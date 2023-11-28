a7db87bf5fdf1668ad62a6e975142c214c33744b
R2sipho <r2sipho@gmail.com>
2023-11-28 06:08:04 -0800

    task

--- 0x01-python-if_else_loops_functions/7-main.py
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+islower = __import__('7-islower').islower
 
-def islower(c):
-    numcheck = ord(c)
-    if numcheck >= 97 and numcheck <= 122:
-        return True
-    else:
-        return False
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
