caa0dc136a465645c2ffeab661591f426eb7b287
R2sipho <r2sipho@gmail.com>
2023-12-04 04:45:53 -0800

    task

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=None):
+    if my_list is None:
+        return
+
+    reversed_list = list(my_list)
+    reversed_list.reverse()
+
+    for i in reversed_list:
+        print("{:d}".format(i))
