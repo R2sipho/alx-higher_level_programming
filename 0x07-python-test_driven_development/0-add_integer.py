8c193c38f360b89c18785e5892ffd6e534fd3bf2
R2sipho <r2sipho@gmail.com>
2024-03-04 07:45:12 -0800

    task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-Module add-integer
-Define a function that adds 2 integers
-"""
+
 def add_integer(a, b=98):
+    """
+    these comments are very stupid
+    """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
-
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
+        raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
+    return int(a) + int(b)    
 
-    return a + b
